6067804b8dac063c44a0f9b0272c95cf2be66786
David Ong <lipweiong@gmail.com>
2023-06-19 10:48:53 +0800

    init catfacts page

---
+++ client/src/pages/Catfacts.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header';
+
+function Catfacts() {
+  return (
+    <div>
+    <Header />
+      Cat Facts
+    </div>
+  )
+}
+
+export default Catfacts
