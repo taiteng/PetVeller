06819716bbe553d69e2452bf65c80a490670f4a9
David Ong <lipweiong@gmail.com>
2023-06-25 15:27:34 +0800

    cat fact api test

--- client/src/pages/Catfacts.jsx
+++ client/src/pages/Catfacts.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
+import axios from 'axios';
+import { Button, Container } from 'react-bootstrap';
+import "../components/css/CatFactStyle.css";
+import Footer from '../components/Footer';
+
 
 function Catfacts() {
+  const [data, setData] = useState([]);
+    const url= `https://catfact.ninja/fact`;
+    const fetchInfo = () => { 
+      return fetch(url) 
+              .then((res) => res.json()) 
+              .then((d) => setData(d)) ;
+      }
+      
+      useEffect(() => {
+        fetchInfo();
+      }, []);
+    
   return (
-    <div>
+    <div style={{ background: 'linear-gradient(to bottom right, #A6BCE8, #FFC0C0)' }}>
     <Header />
-      Cat Facts
-    </div>
+    <br></br>
+    {data.length > 0 ? (
+                <div
+                  className="bg-blue-100 border border-blue-500 rounded p-4 my-4"
+                  style={{ background: 'linear-gradient(to bottom right, #FBE8E8, #FCC2C2)' }}
+                >
+                  <p className="text-lg">{data.fact}</p>
+                </div>
+             
+            ) : (
+              <p className="text-lg">Loading dog facts...</p>
+            )}
+    <center>
+    <button className='button'>Generate a new one</button>
+    </center>
+    <Footer />
+  </div>
   )
 }
 
