0b2dd5900fa926b0b2c8c4991cb33a4bb929340b
David Ong <lipweiong@gmail.com>
2023-06-25 16:36:54 +0800

    cat facts image

--- client/src/pages/Catfacts.jsx
+++ client/src/pages/Catfacts.jsx
@@ -8,28 +8,83 @@ import Footer from '../components/Footer';
 
 function Catfacts() {
   const [data, setData] = useState([]);
-    const url= `https://catfact.ninja/fact`;
+  const [image, setImage] = useState([]);
+
+    const url= `https://catfact.ninja/facts?limit=6`;
+    const imageUrl= `https://api.thecatapi.com/v1/images/search?limit=6&api_key=live_1mcv1m75repApocbaSeLQ0By2z6IF4jvnSLjxDKC2te9S6oNunefBcoSW1upu2nT`;
+   
     const fetchInfo = () => { 
       return fetch(url) 
               .then((res) => res.json()) 
-              .then((d) => setData(d)) ;
+              .then((d) => setData(d.data)) ;
       }
+
+      const fetchImage = () => { 
+        return fetch(imageUrl) 
+                .then((res) => res.json()) 
+                .then((d) => setImage(d)) ;
+        }
       
       useEffect(() => {
         fetchInfo();
+        fetchImage();
       }, []);
+
     
   return (
+    
     <div style={{ background: 'linear-gradient(to bottom right, #A6BCE8, #FFC0C0)' }}>
     <Header />
     <br></br>
-    {data.length > 0 ? (
-                <div
-                  className="bg-blue-100 border border-blue-500 rounded p-4 my-4"
-                  style={{ background: 'linear-gradient(to bottom right, #FBE8E8, #FCC2C2)' }}
-                >
-                  <p className="text-lg">{data.fact}</p>
+    <div id="img-wrapper">
+    {image.length > 0 ? (
+      
+                image.map((images, index) => (
+                  <div
+                    key={index}
+                   
+                  >
+                    <div className='imageBox'>
+                    <img id='image' src={images.url}/>
                 </div>
+                  </div>
+                ))
+                
+            ) : (
+              <p className="text-lg">Loading image...</p>
+            )}
+            </div>
+    
+            {/* <div id="img-wrapper">
+                <div className='imageBox'>
+                    <img src="https://cdn2.thecatapi.com/images/1q7.jpg"/>
+                </div>
+                <div className='imageBox'>
+                    <img src="https://cdn2.thecatapi.com/images/1q7.jpg"/>
+                </div>
+                <div className='imageBox'>
+                    <img src="https://cdn2.thecatapi.com/images/1q7.jpg"/>
+                </div>
+                <div className='imageBox'>
+                    <img src="https://cdn2.thecatapi.com/images/1q7.jpg"/>
+                </div>
+                <div className='imageBox'>
+                    <img src="https://cdn2.thecatapi.com/images/1q7.jpg"/>
+                </div>
+                <div className='imageBox'>
+                    <img src="https://cdn2.thecatapi.com/images/1q7.jpg"/>
+                </div>
+            </div> */}
+    {data.length > 0 ? (
+                data.map((facts, index) => (
+                  <div
+                    key={index}
+                    className="bg-blue-100 border border-blue-500 rounded p-4 my-4"
+                    style={{ background: 'linear-gradient(to bottom right, #FBE8E8, #FCC2C2)' }}
+                  >
+                    <p className="text-lg">{facts.fact}</p>
+                  </div>
+                ))
              
             ) : (
               <p className="text-lg">Loading cat facts...</p>
@@ -37,6 +92,9 @@ function Catfacts() {
     <center>
     <button className='button'>Generate a new one</button>
     </center>
+    <br></br>
+    <br></br>
+    <br></br>
     <Footer />
   </div>
   )
